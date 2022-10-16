1a2929027ae03043def70aa3a4dfc3d942e85177
Martin Maslyankov <m.maslyankov@me.com>
2022-10-16 18:51:01 +0300

    Change some stuff docker_compose script

--- client_env/prepare_docker_compose.sh
+++ client_env/prepare_docker_compose.sh
@@ -5,7 +5,7 @@ sudo apt-get -y install python3-dev
 sudo apt-get install -y python3 python3-pip
 
 echo "Installing docker-compose..."
-sudo pip3 install docker-compose
+sudo pip3 install setuptools_rust docker-compose
 
 echo "Enabling docker on boot"
 sudo systemctl enable docker
