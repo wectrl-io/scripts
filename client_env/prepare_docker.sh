3d96878832e21ddd9b589864627b62b1ade9ed07
Martin Maslyankov <m.maslyankov@me.com>
2022-11-30 23:46:42 +0000

    Update docker install scripts

--- client_env/prepare_docker.sh
+++ client_env/prepare_docker.sh
@@ -2,10 +2,42 @@
 echo "Updating system packages..."
 sudo apt-get update && sudo apt-get upgrade -y
 
-curl -sSl https://get.docker.com | sh
+echo "Uninstalling old versions of docker"
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+echo "Installing dependencies"
+
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+echo "Adding official docker GPG key"
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo "Updating packages list..."
+sudo apt-get update
+
+echo "Installing docker and docker-compose..."
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+echo "Setting up user permissions..."
+sudo groupadd docker
 
 sudo usermod -aG docker ${USER}
 
-echo "Rebooting system..."
-read -t 5 -p "I am going to wait for 5 seconds only ..."
-sudo shutdown -r now
+newgrp docker
+
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+echo "All done! Running a test..."
+
+docker run hello-world
