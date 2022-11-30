0c0ddc0e11177c04768fd990e49c19c9342a0372
Martin Maslyankov <m.maslyankov@me.com>
2022-11-30 23:58:55 +0000

    Updates to docker install script

--- client_env/prepare_docker.sh
+++ client_env/prepare_docker.sh
@@ -1,9 +1,11 @@
 
 echo "Updating system packages..."
 sudo apt-get update && sudo apt-get upgrade -y
+echo ""
 
 echo "Uninstalling old versions of docker"
-sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+echo ""
 
 echo "Installing dependencies"
 
@@ -12,6 +14,7 @@ sudo apt-get install \
     curl \
     gnupg \
     lsb-release
+echo ""
 
 echo "Adding official docker GPG key"
 
@@ -21,23 +24,33 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo""
 
 echo "Updating packages list..."
 sudo apt-get update
+echo ""
 
 echo "Installing docker and docker-compose..."
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+echo ""
 
-echo "Setting up user permissions..."
+echo "Creating docker group..."
 sudo groupadd docker
+echo ""
 
+echo "Adding current user to group docker..."
 sudo usermod -aG docker ${USER}
+echo ""
 
-newgrp docker
-
+echo "Enabling docker services..."
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
+echo ""
 
+echo "Refreshing groups"
+newgrp docker << END
+echo ""
 echo "All done! Running a test..."
-
+echo ""
 docker run hello-world
+END
