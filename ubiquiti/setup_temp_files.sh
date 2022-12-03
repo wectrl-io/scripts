befe8f9696192c71fd94a264cce1b669beba1a48
Martin Maslyankov <m.maslyankov@me.com>
2022-12-03 16:44:58 +0000

    Uncomment forgotten comments

--- ubiquiti/setup_temp_files.sh
+++ ubiquiti/setup_temp_files.sh
@@ -35,51 +35,51 @@ fi
 read -s -p "Password ($user@$target_ip): " password
 echo ""
 
-# if [ 1 -eq $create_wg_interface ]
-#   then
-#     exit
-#     read -p 'External IP: ' ext_ip
-#     echo $ext_ip > /tmp/wg/ext_ip
-#     sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $ext_ip > /tmp/wg/ext_ip"
+if [ 1 -eq $create_wg_interface ]
+  then
+    exit
+    read -p 'External IP: ' ext_ip
+    echo $ext_ip > /tmp/wg/ext_ip
+    sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $ext_ip > /tmp/wg/ext_ip"
 
-#     read -p 'Port to use (ex: 51820): ' port_num
-#     # echo $port_num > /tmp/wg/port_num
-#     sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $port_num > /tmp/wg/port_num"
-
-
-#     read -p 'WG0 IP (ex: 10.0.1.1): ' wg0_ip
-#     # echo $wg0_ip > /tmp/wg/wg0_ip  # "10.0.1.1"
-#     sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $wg0_ip > /tmp/wg/wg0_ip"
-# fi
-
-# read -p 'DNS (ex: 10.1.1.1, 1.1.1.1): ' dns
-# # echo $dns > /tmp/wg/dns
-# sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $dns > /tmp/wg/dns"
-
-# read -p 'peer4 IP (ex: 10.0.1.5): ' peer4_ip
-# # echo $peer4_ip > /tmp/wg/peer4_ip  # "10.0.1.2"
-# sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $peer4_ip > /tmp/wg/peer4_ip"
-
-# read -p 'peer4 Description (ex: "Peer 4"): ' peer4_desc
-# # echo $peer4_desc > /tmp/wg/peer4_desc  # "Peer 4"
-# sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $peer4_desc > /tmp/wg/peer4_desc"
+    read -p 'Port to use (ex: 51820): ' port_num
+    # echo $port_num > /tmp/wg/port_num
+    sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $port_num > /tmp/wg/port_num"
 
 
-# read -p 'peer5 IP (ex: 10.0.1.6): ' peer5_ip
-# # echo $peer5_ip > /tmp/wg/peer5_ip  # "10.0.1.3"
-# sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $peer5_ip > /tmp/wg/peer5_ip"
+    read -p 'WG0 IP (ex: 10.0.1.1): ' wg0_ip
+    # echo $wg0_ip > /tmp/wg/wg0_ip  # "10.0.1.1"
+    sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $wg0_ip > /tmp/wg/wg0_ip"
+fi
 
-# read -p 'peer5 Description (ex: "Peer 5"): ' peer5_desc
-# # echo $peer5_desc > /tmp/wg/peer5_desc  # "Peer 5"
-# sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $peer5_desc > /tmp/wg/peer5_desc"
+read -p 'DNS (ex: 10.1.1.1, 1.1.1.1): ' dns
+# echo $dns > /tmp/wg/dns
+sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $dns > /tmp/wg/dns"
 
-# read -p 'peer6 IP (ex: 10.0.1.7): ' peer6_ip
-# # echo $peer6_ip > /tmp/wg/peer6_ip  # "10.0.1.4"
-# sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $peer6_ip > /tmp/wg/peer6_ip"
+read -p 'peer4 IP (ex: 10.0.1.5): ' peer4_ip
+# echo $peer4_ip > /tmp/wg/peer4_ip  # "10.0.1.2"
+sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $peer4_ip > /tmp/wg/peer4_ip"
 
-# read -p 'peer6 Description (ex: "Peer 6"): ' peer6_desc
-# # echo $peer6_desc > /tmp/wg/peer6_desc  # "Peer 6"
-# sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $peer6_desc > /tmp/wg/peer6_desc"
+read -p 'peer4 Description (ex: "Peer 4"): ' peer4_desc
+# echo $peer4_desc > /tmp/wg/peer4_desc  # "Peer 4"
+sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $peer4_desc > /tmp/wg/peer4_desc"
+
+
+read -p 'peer5 IP (ex: 10.0.1.6): ' peer5_ip
+# echo $peer5_ip > /tmp/wg/peer5_ip  # "10.0.1.3"
+sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $peer5_ip > /tmp/wg/peer5_ip"
+
+read -p 'peer5 Description (ex: "Peer 5"): ' peer5_desc
+# echo $peer5_desc > /tmp/wg/peer5_desc  # "Peer 5"
+sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $peer5_desc > /tmp/wg/peer5_desc"
+
+read -p 'peer6 IP (ex: 10.0.1.7): ' peer6_ip
+# echo $peer6_ip > /tmp/wg/peer6_ip  # "10.0.1.4"
+sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $peer6_ip > /tmp/wg/peer6_ip"
+
+read -p 'peer6 Description (ex: "Peer 6"): ' peer6_desc
+# echo $peer6_desc > /tmp/wg/peer6_desc  # "Peer 6"
+sshpass -p $password ssh -o "StrictHostKeyChecking no" ubnt@$target_ip "echo $peer6_desc > /tmp/wg/peer6_desc"
 
 echo "Prep done! Executing script ($script) on target."
 sshpass -p $password ssh -o "StrictHostKeyChecking no" -t ubnt@$target_ip "/bin/vbash -s" < $target_script
