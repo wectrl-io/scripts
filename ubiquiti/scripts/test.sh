37d562a1fefbd5d56f203f516129019a5988c23a
Martin Maslyankov <m.maslyankov@me.com>
2022-12-25 16:07:30 +0200

    Update ubiquiti test script

--- ubiquiti/scripts/test.sh
+++ ubiquiti/scripts/test.sh
@@ -1,3 +1,12 @@
+#!/bin/vbash
+
+if ! dpkg -s wireguard >/dev/null 2>&1; then
+    echo "WireGuard is not installed."
+else
+    echo "WireGuard is installed."
+fi
+
 echo "Test print!"
 read -p "Test: " test
 echo "You said: $test"
+
